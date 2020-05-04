f6c0026731863bffc5bb58a29ca589533a50befa
Vincent Pham <vincentvpham@gmail.com>
2020-05-04 16:31:37 -0700

    stand down and better tests

--- src/helpers/affiliate.test.ts
+++ src/helpers/affiliate.test.ts
@@ -1,22 +1,34 @@
 import { isAffiliateUrl } from '/helpers/affiliate';
 
-test('parses a URL and knows when an affiliate query key or parameter is present', () => {
-  expect(isAffiliateUrl('domain.com')).toBe(false);
-  expect(isAffiliateUrl('domain.com?q=foobar')).toBe(false);
-  expect(isAffiliateUrl('domain.com?ranEAID=123')).toBe(true);
-  expect(isAffiliateUrl('domain.com?ranMID=123')).toBe(true);
-  expect(isAffiliateUrl('domain.com?ranSiteID=123')).toBe(true);
-  expect(isAffiliateUrl('domain.com?LinkshareID=123')).toBe(true);
-  expect(isAffiliateUrl('domain.com?PartnerID=123')).toBe(false);
-  expect(isAffiliateUrl('domain.com?utm_medium=123')).toBe(false);
-  expect(isAffiliateUrl('domain.com?PartnerID=LINKSHARE')).toBe(true);
-  expect(isAffiliateUrl('domain.com?utm_medium=affiliate')).toBe(true);
-  expect(isAffiliateUrl('domain.com?PartnerID=123&utm_medium=affiliate')).toBe(
-    true,
-  );
-  expect(isAffiliateUrl('domain.com?PartnerID=LINKSHARE&utm_medium=123')).toBe(
-    true,
-  );
-  expect(isAffiliateUrl('domain.com?PartnerID=123&utm_medium=123')).toBe(false);
-  expect(isAffiliateUrl('domain.com?undefined=undefined')).toBe(false);
+describe('isAffiliateUrl()', () => {
+  it('parses a URL and knows when an affiliate query key or parameter is present', () => {
+    expect(isAffiliateUrl('domain.com')).toBe(false);
+    expect(isAffiliateUrl('domain.com?q=foobar')).toBe(false);
+    expect(isAffiliateUrl('domain.com?ranEAID=123')).toBe(true);
+    expect(isAffiliateUrl('domain.com?ranMID=123')).toBe(true);
+    expect(isAffiliateUrl('domain.com?ranSiteID=123')).toBe(true);
+    expect(isAffiliateUrl('domain.com?LinkshareID=123')).toBe(true);
+    expect(isAffiliateUrl('domain.com?PartnerID=123')).toBe(false);
+    expect(isAffiliateUrl('domain.com?utm_medium=123')).toBe(false);
+    expect(isAffiliateUrl('domain.com?PartnerID=LINKSHARE')).toBe(true);
+    expect(isAffiliateUrl('domain.com?utm_medium=affiliate')).toBe(true);
+    expect(
+      isAffiliateUrl('domain.com?PartnerID=123&utm_medium=affiliate'),
+    ).toBe(true);
+    expect(
+      isAffiliateUrl('domain.com?PartnerID=LINKSHARE&utm_medium=123'),
+    ).toBe(true);
+    expect(isAffiliateUrl('domain.com?PartnerID=123&utm_medium=123')).toBe(
+      false,
+    );
+    expect(isAffiliateUrl('domain.com?undefined=undefined')).toBe(false);
+    expect(
+      isAffiliateUrl('domain.com?foid=177.142931376&foc=1&fot=9999&fos=1'),
+    ).toBe(true);
+    expect(isAffiliateUrl('domain.com?foc=1&fot=9999&fos=1')).toBe(false);
+    expect(isAffiliateUrl('domain.com?afsrc=1')).toBe(true);
+    expect(isAffiliateUrl('domain.com?afsrc=2')).toBe(false);
+    expect(isAffiliateUrl('domain.com?affsource=1')).toBe(true);
+    expect(isAffiliateUrl('domain.com?affsource=2')).toBe(false);
+  });
 });
--- src/helpers/affiliate.ts
+++ src/helpers/affiliate.ts
@@ -1,12 +1,21 @@
 import { parseUrl } from 'query-string';
 
 // affiliated if any of these query parameter keys are present
-const targetQueryKeys = ['ranEAID', 'ranMID', 'ranSiteID', 'LinkshareID'];
+const targetQueryKeys = [
+  'ranEAID',
+  'ranMID',
+  'ranSiteID',
+  'LinkshareID',
+  'foid',
+];
 
 // affiliated if any of these query parameters (key and value) are present together
+// value must be a string - parseUrl returns strings
 const targetQueryParams: { [queryKey: string]: string } = {
   PartnerID: 'LINKSHARE',
   utm_medium: 'affiliate',
+  afsrc: '1',
+  affsource: '1',
 };
 
 export const isAffiliateUrl = (url: string): boolean => {
